cec73b716d6c931fc85edd4c3c9d2cc01822e837
labuser <labuser@ip-80-161.wireless.oberlin.edu>
2018-10-04 21:26:14 -0400

    ...

--- week05/random_di/random_di.ino
+++ week05/random_di/random_di.ino
@@ -1,4 +1,12 @@
 void setup() {
+
+  // turn the pins on!
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+
   // lets us use println
   Serial.begin(9600);
 
@@ -13,13 +21,62 @@ void setup() {
 
   // use this as our random seed
   randomSeed(analogValue);
+
+  // generate a random number 0->5
+  int rando = random(6);
+  Serial.println(rando);  // print it in console
+
+  // if random number generates a 0...
+  if (rando == 0) {
+    digitalWrite(3, LOW); // dont turn any on
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+  }
+  // if random number generates a 1...
+  if (rando == 1) {
+    digitalWrite(3, HIGH); // turn one on
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+  }
+  if (rando == 2) {
+    digitalWrite(3, HIGH); // turn two on
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+  }
+  if (rando == 3) {
+    digitalWrite(3, HIGH); // turn three on
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+  }
+  if (rando == 4) {
+    digitalWrite(3, HIGH); // turn four on
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, LOW);
+  }
+  if (rando == 5) {
+    digitalWrite(3, HIGH); // turn five on
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+  }
 }
 
 void loop() {
-  // generate a random number 0->9
-  int rando = random(10);
-  Serial.println(rando);  // print it in console
 
-  delay(1000);  // wait a sec
+
+
+
+  //delay(1000);  // wait a sec
 
 }
